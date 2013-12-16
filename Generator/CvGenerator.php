3d0988a34680dc02f59044ede530a04df360972c
mhor <maxime.horcholle@gmail.com>
2013-12-16 21:26:18 +0100

    little modifs

--- Generator/CvGenerator.php
+++ Generator/CvGenerator.php
@@ -1,10 +1,34 @@
 <?php
+
 namespace Mhor\CvToPdfBundle\Generator;
+
+use Mhor\CvToPdfBundle\Model\Cv;
 
 class CvGenerator
 {
-    public function generateCv()
+    public function generateCv(Cv $cv)
     {
         return "Service not yet implemented\n";
+    }
+
+    /**
+     * Transform XML to Cv Object
+     * 
+     * @param File $xmlCv
+     * @return \Mhor\CvToPdfBundle\Model\Cv
+     */
+    public function transformXmlCvToObject($xmlCv)
+    {
+        return new Cv();
+    }
+
+    /**
+     * Transform JSON to Cv Object
+     * @param File $xmlJson
+     * @return \Mhor\CvToPdfBundle\Model\Cv
+     */
+    public function transformJsonCvToObject($xmlJson)
+    {
+        return new Cv();
     }
 }
